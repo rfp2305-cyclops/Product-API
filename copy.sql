d9706da9c0fd86c3134014e920178bf48fb13084
Holly Stefan <hmbullis828@gmail.com>
2023-07-12 14:02:00 -0600

    loaded all data into database

---
+++ copy.sql
@@ -0,0 +1,24 @@
+COPY products(id, name, slogan, description, category, default_price)
+FROM '/Users/hollystefan/Desktop/Hackreactor/Course/SDC/Product-API/seedData/product.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY styles(id, product_id, name, sale_price, original_price, default_style)
+FROM '/Users/hollystefan/Desktop/Hackreactor/Course/SDC/Product-API/seedData/styles.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY skus(id, style_id, size, quantity)
+FROM '/Users/hollystefan/Desktop/Hackreactor/Course/SDC/Product-API/seedData/skus.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY photos(id, style_id, url, thumbnail_url)
+FROM '/Users/hollystefan/Desktop/Hackreactor/Course/SDC/Product-API/seedData/photos.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY features(id, product_id, feature, value)
+FROM '/Users/hollystefan/Desktop/Hackreactor/Course/SDC/Product-API/seedData/features.csv'
+DELIMITER ','
+CSV HEADER;
