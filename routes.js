5d787f1deafde6616e4fe67ff9dce55a9908d212
Holly Stefan <hmbullis828@gmail.com>
2023-07-14 14:38:07 -0600

    all routes working

--- routes.js
+++ routes.js
@@ -1,23 +1,13 @@
-// const controller = require("./controllers.js");
+const controller = require("./controllers.js");
 const router = require("express").Router();
-const db = require("./connection.js");
+//const db = require("./connection.js");
 
-router.get("/", async (req, res) => {
-  const qrystr = `SELECT * FROM products WHERE id = 1`;
-  try {
-    const client = await db.pool.connect();
-    const products = await client.query(qrystr);
-    client.release();
-    res.status(200).send(products);
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.get("/", controller.products);
 
-// router.get("/:product_id", controller.product);
+router.get("/:product_id", controller.product);
 
-// router.get("/:product_id/styles", controller.styles);
+router.get("/:product_id/styles", controller.styles);
 
-// router.get("/:product_id/related", controller.related);
+router.get("/:product_id/related", controller.related);
 
 module.exports = router;
